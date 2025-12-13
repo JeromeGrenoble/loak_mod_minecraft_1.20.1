0f65634a87ad1c4442179c744f45055ae9d96347
JeromeGrenoble <145987401+JeromeGrenoble@users.noreply.github.com>
2025-12-13 02:56:07 +0100

    Fix: add/Entity_not_working

--- src/main/java/net/Skin0oz/loakmod/event/ModEventBusEvents.java
+++ src/main/java/net/Skin0oz/loakmod/event/ModEventBusEvents.java
@@ -1,12 +1,10 @@
 package net.Skin0oz.loakmod.event;
 
-import com.google.common.eventbus.Subscribe;
 import net.Skin0oz.loakmod.LOAKMod;
 import net.Skin0oz.loakmod.entity.ModEntities;
 import net.Skin0oz.loakmod.entity.client.ModModelLayers;
 import net.Skin0oz.loakmod.entity.client.RhinoModel;
 import net.Skin0oz.loakmod.entity.custom.RhinoEntity;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -14,9 +12,19 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = LOAKMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEventBusEvents {
+
+    // Enregistrement des attributs des entités
     @SubscribeEvent
     public static void registerAttributes(EntityAttributeCreationEvent event) {
         event.put(ModEntities.RHINO.get(), RhinoEntity.createAttributes().build());
     }
 
+    // Enregistrement des LayerDefinitions pour les modèles
+    @SubscribeEvent
+    public static void registerLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(
+                ModModelLayers.RHINO_LAYER,
+                RhinoModel::createBodyLayer
+        );
+    }
 }
